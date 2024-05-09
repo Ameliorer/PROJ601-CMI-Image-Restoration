d59d3a5be28b1a269706095f4c4be731c421d338
Ameliorer <ameliehacque@gmail.com>
2024-05-09 15:07:19 +0200

    diffusion anisotropique et conductance qui fonctionnent avec des commentaires et des tests

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import math
+
 import cv2
 
 
-def diffusion_anisotrope(image, dt, conductance):
+def diffusion_anisotrope(image, dt, conductance, kappa, option):
     """
     Cette fonction applique une diffusion anisotrope sur une image.
 
-    Args:
-        image: L'image d'entrée sous forme de tableau NumPy.
-        dt: Pas de temps pour le processus de diffusion.
-        conductance: Fonction qui calcule le coefficient de diffusion à chaque pixel.
+    Args :
+        image : L'image d'entrée sous forme de tableau NumPy.
+        dt : Pas de temps pour le processus de diffusion.
+        conductance : Fonction qui calcule le coefficient de diffusion à chaque pixel.
 
-    Returns:
+    Returns :
         L'image diffusée sous forme de tableau NumPy.
     """
 
@@ -18,6 +20,7 @@ def diffusion_anisotrope(image, dt, conductance):
     image_diffusee = image.copy()
 
     # Parcourir chaque pixel de l'image sauf les bordures
+    # On ne traite pas les bordures, car on impose leur conduction à 0. Ce qui veut dire que le pixel ne change pas
     for i in range(1, image.shape[0] - 1):
         for j in range(1, image.shape[1] - 1):
             # Obtenir les valeurs des pixels voisins
@@ -29,85 +32,93 @@ def diffusion_anisotrope(image, dt, conductance):
             }
 
             # Calculer les gradients vers les voisins
+            # Voir les équations (8) de l'article
             grad_n = voisins["W"] - image[i, j]
             grad_e = voisins["N"] - image[i, j]
             grad_s = voisins["E"] - image[i, j]
             grad_w = voisins["S"] - image[i, j]
 
             # Calculer les coefficients de diffusion en utilisant la fonction de conductance
-            conductance_n = conductance(grad_n)
-            conductance_e = conductance(grad_e)
-            conductance_s = conductance(grad_s)
-            conductance_w = conductance(grad_w)
+            # Voir les équations (10) de l'article
+            conductance_n = conductance(abs(grad_n), kappa, option)
+            conductance_e = conductance(abs(grad_e), kappa, option)
+            conductance_s = conductance(abs(grad_s), kappa, option)
+            conductance_w = conductance(abs(grad_w), kappa, option)
 
             # Calculer les différences finies
+            # Voir l'équation (7) de l'article
             delta_n = conductance_n * grad_n
             delta_e = conductance_e * grad_e
             delta_s = conductance_s * grad_s
             delta_w = conductance_w * grad_w
 
             # Mettre à jour la valeur du pixel en fonction des gradients et des coefficients de diffusion
+            # Voir l'équation (7) de l'article
             image_diffusee[i, j] += dt * (delta_n + delta_e + delta_s + delta_w)
 
     return image_diffusee
 
 
-def conductance(gradient):
+def conductance(gradient, kappa, option):
     """
-  Cette fonction calcule le coefficient de diffusion en fonction du gradient.
+    Cette fonction calcule le coefficient de diffusion en fonction du gradient.
 
-  Args:
-      gradient: La grandeur du gradient de l'image sous forme de tableau NumPy.
+    Args :
+        gradient : La grandeur du gradient de l'image sous forme de tableau NumPy.
+        kappa :
+        option :
 
-  Returns:
-      Un tableau NumPy contenant le coefficient de diffusion pour chaque pixel.
-  """
-    # Définir les paramètres de conductance
+        Les espaces d'échelle générés par ces deux fonctions sont différents :
+        le premier privilégie les arêtes à fort contraste par rapport à celles à faible contraste,
+        tandis que le second privilégie les régions larges par rapport aux plus petites.
 
-    # Calculer le coefficient de diffusion
-    c = 1.0 / (1.0 + (gradient / 15) ** 2)
+    Returns :
+        Un tableau NumPy contenant le coefficient de diffusion pour chaque pixel.
+    """
+
+    if option == 2:
+        c = math.exp(-(gradient / kappa) ** 2)
+    else:
+        # Calculer le coefficient de diffusion
+        c = 1.0 / (1.0 + (gradient / kappa) ** 2)
 
     return c
 
 
-# Charger l'image et la convertir en niveaux de gris
-filename = ("mandrill-g2.png")
-img = cv2.imread(filename)
-imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-imgGrey = imgGrey.astype('float64')
+def main(filename_originale, filenames_bruite, dt, iterations, kappa, option):
+    # Charger l'image originale et la convertir en niveaux de gris
+    img_originale = cv2.imread(filename_originale)
+    img_originale = cv2.cvtColor(img_originale, cv2.COLOR_BGR2GRAY)
+    img_originale = img_originale.astype('float64')
+    cv2.imshow("Image originale", img_originale.astype('uint8'))
+    cv2.waitKey(0)
 
-# Appliquer le flou gaussien pour réduire le bruit
-gaussian = cv2.GaussianBlur(imgGrey, (5, 5), 0)
+    for filename_bruite in filenames_bruite:
+        # Charger l'image bruitée et la convertir en niveaux de gris
+        img = cv2.imread(filename_bruite)
+        imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        imgGrey = imgGrey.astype('float64')
 
-derivX = cv2.Sobel(gaussian,ddepth=-1,dx=1,dy=0)
-derivY = cv2.Sobel(gaussian,ddepth=-1,dx=0,dy=1)
-abs_grad_x = cv2.convertScaleAbs(derivX)
-abs_grad_y = cv2.convertScaleAbs(derivY)
-grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+        # Appliquer le flou gaussien pour réduire le bruit
+        gaussian = cv2.GaussianBlur(imgGrey, (5, 5), 0)
 
-# Définir les paramètres de diffusion
+        # Diffuser l'image de manière itérative
+        image_finale = gaussian
+        for _ in range(iterations):
+            image_finale = diffusion_anisotrope(image_finale, dt, conductance, kappa, option)
+
+        # Afficher les résultats
+        cv2.imshow(filename_bruite, imgGrey.astype('uint8'))
+        cv2.imshow(f"{filename_bruite} diffusée", image_finale.astype('uint8'))
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+# Valeurs pour les tests
+filename_originale = "femme.jpg"
+filenames_bruite = ["femme.jpg", "femme-noisy50.png", "femme-noisy80.png", "femme-noisy100.png"]
 dt = 0.14
-iterations = 10  # Nombre d'itérations de diffusion
-
-# Diffuser l'image de manière itérative
-image_finale = gaussian
-for _ in range(iterations):
-    image_finale = diffusion_anisotrope(image_finale, dt, conductance)
-
-
-derivX = cv2.Sobel(image_finale,ddepth=-1,dx=1,dy=0)
-derivY = cv2.Sobel(image_finale,ddepth=-1,dx=0,dy=1)
-abs_grad_x = cv2.convertScaleAbs(derivX)
-abs_grad_y = cv2.convertScaleAbs(derivY)
-gradfinal = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-
-# Afficher les résultats
-cv2.imshow("Image originale", imgGrey.astype('uint8'))
-cv2.imshow("Image diffusée", image_finale.astype('uint8'))
-cv2.imshow("petit Flou Gaussien", (cv2.GaussianBlur(imgGrey, (3,3), 0)).astype('uint8'))
-cv2.imshow("grand Flou Gaussien", (cv2.GaussianBlur(imgGrey, (9,9), 0)).astype('uint8'))
-cv2.imshow("gradient", grad.astype('uint8'))
-cv2.imshow("gradient final", gradfinal.astype('uint8'))
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+iterations = 30
+kappa = 5
+option = 2
+main(filename_originale, filenames_bruite, dt, iterations, kappa, option)
